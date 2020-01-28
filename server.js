188d6ca4c7eac381a9e85c3a69d54e79142c7eac
Daphne <daphne.feresin@gmail.com>
2020-01-28 13:45:08 -0300

    updates

--- server.js
+++ server.js
@@ -1,257 +1,58 @@
 const express = require ("express")
 const app = express()
 
-const apps = [
-    {
-        id : 50,
-        name: "my-app-050"
-    },
-    {
-        id : 51,
-        name: "my-app-051"
-    },
-    {
-        id : 52,
-        name: "my-app-052"
-    },
-    {
-        id : 53,
-        name: "my-app-053"
-    },
-    {
-      id : 1,
-      name: "my-app-001"
-    },
-    {
-      id : 2,
-      name: "my-app-002"
-    },
-    
-    {
-        id : 7,
-        name: "my-app-007"
-    },
-    {
-        id : 8,
-        name: "my-app-008"
-    },
-    {
-        id : 9,
-        name: "my-app-009"
-    },
-    {
-        id : 10,
-        name: "my-app-010"
-    },
-    {
-        id : 12,
-        name: "my-app-012"
-    },
-    
-    {
-        id : 16,
-        name: "daphne"
-    },
-    {
-        id : 17,
-        name: "my-app-017"
-      },
-      {
-        id : 18,
-        name: "my-app-018"
-      },
-      {
-      id : 19,
-      name: "my-app-019"
-      },
-      {
-          id : 20,
-          name: "my-app-020"
-      },
-      {
-          id : 21,
-          name: "my-app-021"
-      },
-      {
-          id : 22,
-          name: "my-app-022"
-      },
-      {
-          id : 23,
-          name: "my-app-023"
-      },
-      {
-          id : 24,
-          name: "my-app-025"
-      },
-      {
-        id : 3,
-        name: "my-app-003"
-        },
-        {
-            id : 4,
-            name: "my-app-004"
-        },
-        {
-            id : 42,
-            name: "my-app-042"
-        },
-        {
-            id : 43,
-            name: "my-app-043"
-        },
-        {
-            id : 44,
-            name: "my-app-044"
-        },
-        {
-            id : 45,
-            name: "my-app-045"
-        },
-        {
-            id : 46,
-            name: "my-app-046"
-        },
-        {
-            id : 5,
-            name: "my-app-005"
-        },
-        {
-            id : 6,
-            name: "my-app-006"
-        },
-      {
-          id : 26,
-          name: "my-app-027"
-      },
-      {
-          id : 28,
-          name: "my-app-029"
-      },
-      {
-          id : 29,
-          name: "my-app-029"
-      },
-      {
-          id : 30,
-          name: "my-app-030"
-      },
-      {
-          id : 31,
-          name: "my-app-031"
-      },
-      {
-          id : 32,
-          name: "my-app-032"
-      },
-      {
-          id : 33,
-          name: "my-app-034"
-      },
-      {
-        id : 35,
-        name: "my-app-035"
-      },
-      {
-        id : 36,
-        name: "my-app-036"
-      },
-      {
-      id : 37,
-      name: "my-app-037"
-      },
-      {
-          id : 38,
-          name: "my-app-038"
-      },
-      {
-          id : 39,
-          name: "my-app-039"
-      },
-      {
-          id : 40,
-          name: "my-app-040"
-      },
-      {
-          id : 41,
-          name: "my-app-041"
-      },
-      
-      {
-          id : 47,
-          name: "my-app-047"
-      },
-      {
-          id : 48,
-          name: "my-app-048"
-      },
-      {
-          id : 49,
-          name: "my-app-049"
-      },
-      {
-        id : 13,
-        name: "my-app-013"
-    },
-    {
-        id : 14,
-        name: "my-app-014"
-    },
-    {
-        id : 15,
-        name: "my-app-015"
-    },
-    {
-        id : 11,
-        name: "my-app-011"
-    },
-    {
-        id : 25,
-        name: "my-app-025"
-    },
-    {
-        id : 27,
-        name: "my-app-027"
-    },
-    {
-        id : 34,
-        name: "my-app-034"
-    },
-  
-  ];
-
-
-
+//assigning data from seed.js fle to apps variable 
+const apps = require('./seed').apps;
 
 
 app.get('/apps',(req,res)=>{
+
+//will manipulate apps array from seed file according to params and assign it to allApps   
 let allApps =[]
- 
+
+//--------------defining range variables---------------
 let by = req.query.by
-let start = req.query.start||1
+let start = req.query.start||0
 let end = req.query.end 
 const max = req.query.max||50
-let order = req.query.order 
+let order = req.query.order||"ASC" 
+//......................................................
 
 
-if(by=="id"){
-    if(order=="desc"){
-        allApps = apps.sort(function(a, b){return b.id-a.id});
+//checking if start was entered as a Name, and looking for that name in the array
+if(start!=0 && start.slice(0,2)=="my"){
+    let findStartName = apps.find(arr => arr.name == start); 
+//saving index of the name found in start variable
+    start = apps.indexOf(findStartName)
+}
+//checking if start was entered as an ID, and looking for that ID in the array
+else if(start!=0){
+    let findStartId = apps.find(arr => arr.id == start); 
+//saving index of the ID found in start variable
+    start = apps.indexOf(findStartId)
+}
+
+//if key "by" equals id, I sort the apps array by id according to "order", where "asc" is default
+if(by =="id"){
+//I convert order to upper case to compare in case the user enters capital letters
+    if(order.toUpperCase()=="DESC"){ 
+        allApps = apps.sort(function(a, b){return b.id-a.id}); 
     }
-    else{
-
+    else if(order.toUpperCase()=="ASC"){
         allApps = apps.sort(function(a, b){return a.id-b.id});
     }
     }
+//......................sort by ID ends................................
+//if key "by" equals name, I sort the apps array alphabetically by name according to "order", where "asc" is default
     else if(by =="name"){
-    if(order=="desc"){
+    if(order.toUpperCase()=="DESC"){
         allApps = apps.sort(function(a, b){
             if(b.name < a.name) { return -1; }
             if(b.name > a.name) { return 1; }
             return 0;}
             );
     }
-    else{
+    else if(order.toUpperCase()=="ASC"){
         allApps = apps.sort(function(a, b){
             if(a.name < b.name) { return -1; }
             if(a.name > b.name) { return 1; }
@@ -259,15 +60,37 @@ if(by=="id"){
             );
     }
     }
+//.....................sort by name ends...................................
+//sorted array was assigned to AllApps variable
 
-let final = allApps 
-let finalMax
+//I calculate max total based on start 
+let finalMax   
+finalMax = Number(start) + Number(max)
 
-if(start || max){
-finalMax = Number(start-1) + Number(max)
-        final = allApps.slice(start-1,finalMax)
+
+
+//check if there is an "end" parameter and search for it in the array (name or id)
+if(end){
+    if(end.slice(0,2)=="my"){
+        let findEndName = apps.find(arr => arr.name == end); 
+//saving index of the name found in end variable
+        end = apps.indexOf(findEndName)
+    }   
+    else if(end!=0){
+        let findEndId = apps.find(arr => arr.id == end); 
+//saving index of the ID found in end variable
+        end = apps.indexOf(findEndId)        
+    }  
+    if(end<finalMax){
+        finalMax = end+1;
     }
+}
 
+
+
+
+//slice the array according to start and max and send final sliced array as a json response
+let final = allApps.slice(start,finalMax) 
       res.json(final)
     })
 
